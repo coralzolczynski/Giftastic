55db5a043c3214341c1cab882cd5c2028c37e360
unknown <cmzolczynski@gmail.com>
2019-06-12 13:19:03 -0700

    added bcg image and css

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -83,13 +83,13 @@ $("button").on("click", function() {
   });
 
 
-//   $(".gif").on("click", function() {
-//     var state = $(this).attr("data-state");
-//     if (state === 'still') {
-//         $(this).attr('src', $(this).attr('data-animate'));
-//         $(this).attr('data-state', 'animate');
-//       } else {
-//         $(this).attr('src', $(this).attr('data-still'));
-//         $(this).attr('data-state', 'still');
-//       }
-//     });
+  $(".gif").on("click", function() {
+    var state = $(this).attr("data-state");
+    if (state === 'still') {
+        $(this).attr('src', $(this).attr('data-animate'));
+        $(this).attr('data-state', 'animate');
+      } else {
+        $(this).attr('src', $(this).attr('data-still'));
+        $(this).attr('data-state', 'still');
+      }
+    });
